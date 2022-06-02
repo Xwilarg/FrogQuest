498a5699fc5deb8a2d52c35f63321815e6953db0
Xwilarg <xwilarg@protonmail.com>
2022-06-02 18:46:25 +0200

    Add enemy turn

--- Assets/Scripts/Game/TurnManager.cs
+++ Assets/Scripts/Game/TurnManager.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using TouhouPrideGameJam4.Character;
 using TouhouPrideGameJam4.Map;
+using TouhouPrideGameJam4.SO;
 using UnityEngine;
 
 namespace TouhouPrideGameJam4.Game
@@ -9,6 +10,9 @@ namespace TouhouPrideGameJam4.Game
     public class TurnManager : MonoBehaviour
     {
         public static TurnManager Instance { get; private set; }
+
+        [SerializeField]
+        private AIInfo _aiInfo;
 
         public ACharacter Player { set; private get; }
         private List<ACharacter> _enemies = new();
@@ -53,10 +57,45 @@ namespace TouhouPrideGameJam4.Game
             if (target != null) // Enemy on the way, we attack it
             {
                 Player.Attack(target);
+                PlayerEnemyTurn();
             }
             else if (MapManager.Instance.IsTileWalkable(newX, newY)) // Nothing here, we can move
             {
                 Player.Position = new(newX, newY);
+                PlayerEnemyTurn();
+            }
+        }
+
+        private void PlayerEnemyTurn()
+        {
+            Vector2Int[] directions = new[]
+            {
+                Vector2Int.left, Vector2Int.right, Vector2Int.up, Vector2Int.down
+            };
+            foreach (var enemy in _enemies)
+            {
+                if (Vector2.Distance(enemy.Position, Player.Position) < _aiInfo.MaxDistanceToMove)
+                {
+                    var dirTarget = directions.OrderBy(d => Vector2.Distance(enemy.Position + d, Player.Position));
+                    foreach (var d in dirTarget)
+                    {
+                        if (_enemies.FirstOrDefault(e => e.Position.x == enemy.Position.x + d.x && e.Position.y == enemy.Position.y + d.y))
+                        {
+                            // An enemy is obstructing the way
+                            continue;
+                        }
+                        if (Player.Position.x == enemy.Position.x + d.x && Player.Position.y == enemy.Position.y + d.y)
+                        {
+                            enemy.Attack(Player);
+                            break;
+                        }
+                        else if (MapManager.Instance.IsTileWalkable(enemy.Position.x + d.x, enemy.Position.y + d.y))
+                        {
+                            enemy.Position = new(enemy.Position.x + d.x, enemy.Position.y + d.y);
+                            break;
+                        }
+                    }
+                }
             }
         }
     }
