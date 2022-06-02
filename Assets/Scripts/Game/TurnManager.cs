031ceaf4ecc48c6421193b63bdedf33b44e0cffd
Xwilarg <xwilarg@protonmail.com>
2022-06-02 17:51:12 +0200

    Prevent player to walk on enemies

--- Assets/Scripts/Game/TurnManager.cs
+++ Assets/Scripts/Game/TurnManager.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using TouhouPrideGameJam4.Character;
 using TouhouPrideGameJam4.Map;
 using UnityEngine;
@@ -27,7 +28,11 @@ namespace TouhouPrideGameJam4.Game
             var newX = Player.Position.x + relX;
             var newY = Player.Position.y + relY;
 
-            if (MapManager.Instance.IsTileWalkable(newX, newY))
+            if (_enemies.Any(e => e.Position.x == newX && e.Position.y == newY)) // Enemy on the way, we attack it
+            {
+                // TODO
+            }
+            else if (MapManager.Instance.IsTileWalkable(newX, newY)) // Nothing here, we can move
             {
                 Player.Position = new(newX, newY);
             }
