88ea43cad6d6c3045cef01a428d84db0516ec4cf
Xwilarg <xwilarg@protonmail.com>
2022-06-12 18:03:31 +0200

    Add UI script

---
+++ Assets/Scripts/Menu/MenuButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace TouhouPrideGameJam4.Menu
+{
+    public class MenuButton : MonoBehaviour, IPointerEnterHandler
+    {
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+        }
+    }
+}
