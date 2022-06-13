076250b31502f5847019c2231d81b59c7a49e4ea
Xwilarg <xwilarg@protonmail.com>
2022-06-13 03:56:05 +0200

    Add main menu

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace TouhouPrideGameJam4.Menu
+{
+    public class MenuManager : MonoBehaviour
+    {
+        [SerializeField]
+        private Button _exitButton;
+
+        private void Start()
+        {
+            if (Application.platform == RuntimePlatform.WebGLPlayer)
+            {
+                _exitButton.gameObject.SetActive(false);
+            }
+        }
+
+        public void Play()
+        {
+            SceneManager.LoadScene("Main");
+        }
+
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
