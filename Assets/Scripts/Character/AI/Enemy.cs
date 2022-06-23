372509e1d8077399033a591638032de1eac17dd5
Xwilarg <xwilarg@protonmail.com>
2022-06-23 20:20:45 +0200

    Improve Remilia teleportation

--- Assets/Scripts/Character/AI/Enemy.cs
+++ Assets/Scripts/Character/AI/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using TouhouPrideGameJam4.Game;
+﻿using TouhouPrideGameJam4.Character.Player;
+using TouhouPrideGameJam4.Game;
 using TouhouPrideGameJam4.Map;
 using TouhouPrideGameJam4.SO.Item;
 using UnityEngine;
@@ -26,9 +27,17 @@ namespace TouhouPrideGameJam4.Character.AI
                 int x, y;
                 do
                 {
-                    x = Random.Range(Position.x - 5, Position.x + 6);
-                    y = Random.Range(Position.y - 5, Position.y + 6);
-                } while (!MapManager.Instance.IsTileWalkable(x, y) || TurnManager.Instance.GetCharacterPos(x, y) != null);
+                    x = PlayerController.Instance.Position.x;
+                    y = PlayerController.Instance.Position.y;
+                    if (Random.Range(0, 2) == 0)
+                    {
+                        x = Random.Range(x - 5, x + 6);
+                    }
+                    else
+                    {
+                        y = Random.Range(y - 5, y + 6);
+                    }
+                } while (Vector2Int.Distance(new(x, y), PlayerController.Instance.Position) <= 2 || !MapManager.Instance.IsTileWalkable(x, y) || TurnManager.Instance.GetCharacterPos(x, y) != null);
                 Position = new(x, y);
             }
         }
