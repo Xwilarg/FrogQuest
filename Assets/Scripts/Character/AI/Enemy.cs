525a07c83b0b9c2223578890bd75db7a6a0ef564
zanejgr <zanejgr>
2022-06-29 16:57:31 -0500

    Working status symbols on enemies
    Status symbols correctly show
    enemy statuses. Maybe introduced new bugs.
    Need to test before pull request.

--- Assets/Scripts/Character/AI/Enemy.cs
+++ Assets/Scripts/Character/AI/Enemy.cs
@@ -1,4 +1,5 @@
-﻿using TouhouPrideGameJam4.Character.Player;
+﻿using System.Linq;
+using TouhouPrideGameJam4.Character.Player;
 using TouhouPrideGameJam4.Game;
 using TouhouPrideGameJam4.Map;
 using TouhouPrideGameJam4.SO.Item;
@@ -8,9 +9,43 @@ namespace TouhouPrideGameJam4.Character.AI
 {
     public class Enemy : ACharacter
     {
+        /// <summary>
+        /// Status symbol sprites
+        /// </summary>
+        private GameObject BindSprite, FreezeSprite, StunSprite, InvulnerableSprite;
         private void Start()
         {
             Init(Team);
+        }
+        private void Awake() {
+            foreach(Transform child in this.transform){
+                //if I don't know what the child is, don't change activity
+                bool wasActive= child.gameObject.activeSelf;
+                child.gameObject.SetActive(false);
+                if(child.name.Contains("Bind")){
+                    BindSprite=child.gameObject;
+                }
+                else if (child.name.Contains("Frozen") || child.name.Contains("Freeze")){
+                    FreezeSprite = child.gameObject;
+                }
+                else if (child.name.Contains("Stun")){
+                    StunSprite = child.gameObject;
+                }
+                else if (child.name.Contains("Invulnerability") || child.name.Contains("Invulnerable")){
+                    InvulnerableSprite = child.gameObject;
+                }else{
+                    child.gameObject.SetActive(wasActive);
+                }
+            }
+            
+        }
+        public override void OnStatusChange()
+        {
+            base.OnStatusChange();
+            BindSprite?.SetActive(_currentEffects.ContainsKey(StatusType.Binded));
+            FreezeSprite?.SetActive(_currentEffects.ContainsKey(StatusType.Frozen));
+            StunSprite?.SetActive(_currentEffects.ContainsKey(StatusType.Stunned));
+            InvulnerableSprite?.SetActive(_currentEffects.ContainsKey(StatusType.Invincible));
         }
 
         private void Update()
